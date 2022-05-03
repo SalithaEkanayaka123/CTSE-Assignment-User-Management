6bbc07c5b652cc91ded36705df6247a0c6aee6e1
Salitha Ekanayaka <68500118+SalithaEkanayaka123@users.noreply.github.com>
2022-05-03 18:56:54 +0530

    Found a Way to reupload changes to docker image

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -14,6 +14,11 @@ public class DemoApplication {
         return "Hello Docker World";
     }
 
+    @RequestMapping("/home1")
+    public String home1() {
+        return "Testing";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
