5a0f93a5b94ad1b9922fd217871b4a2ea0bed103
Salitha Ekanayaka <68500118+SalithaEkanayaka123@users.noreply.github.com>
2022-05-05 02:48:15 +0530

    Cart Model Cart Updated

--- src/main/java/com/example/demo/model/Cart.java
+++ src/main/java/com/example/demo/model/Cart.java
@@ -1,9 +1,66 @@
 package com.example.demo.model;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cartItem")
 public class Cart {
-    //id
-    //cartNo
-    //userID
-    //itemID
-    //item count
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "cartNo")
+    private String cartNo;
+
+    @Column(name = "userID")
+    private String userID;
+
+    @Column(name = "itemID")
+    private String itemID;
+
+    @Column(name = "item count")
+    private int item_count;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCartNo() {
+        return cartNo;
+    }
+
+    public void setCartNo(String cartNo) {
+        this.cartNo = cartNo;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getItemID() {
+        return itemID;
+    }
+
+    public void setItemID(String itemID) {
+        this.itemID = itemID;
+    }
+
+    public int getItem_count() {
+        return item_count;
+    }
+
+    public void setItem_count(int item_count) {
+        this.item_count = item_count;
+    }
 }
