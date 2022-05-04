287401e485ad2a153e46d5046749bde340532eb4
Salitha Ekanayaka <68500118+SalithaEkanayaka123@users.noreply.github.com>
2022-05-04 16:43:29 +0530

    add user added

--- src/main/java/com/example/demo/Controller/userController.java
+++ src/main/java/com/example/demo/Controller/userController.java
@@ -4,9 +4,10 @@ package com.example.demo.Controller;
 import com.example.demo.Repository.userRepository;
 import com.example.demo.model.Users;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,4 +23,21 @@ public class userController {
     public List<Users> getAllEmployees() {
         return userRepository.findAll();
     }
+
+
+    @PostMapping("/useradd")
+    public ResponseEntity<?> createUser(@RequestBody Users user) {
+        try {
+            System.out.println("user is " + user.getPassword());
+            userRepository.save(user);
+            return new ResponseEntity<Users>(user, HttpStatus.OK);//
+        } catch (DataIntegrityViolationException e){
+            return new ResponseEntity<>("username exists", HttpStatus.OK);
+        }catch (Exception e){
+            System.out.println("si " + e);
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.FORBIDDEN);
+        }
+    }
+
+
 }
